3f6dbf7aa91c08baaa60e09d907e76da5b3d52b5
Abdelrahman Omran <me@omranic.com>
2017-02-11 16:29:43 +0200

    Refactor frontend layer and move to web pack from gulp

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,20 +1,61 @@
 
 /**
  * First we will load all of this project's JavaScript dependencies which
- * include Vue and Vue Resource. This gives a great starting point for
+ * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+window._ = require('lodash');
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the body of the page. From here, you may begin adding components to
- * the application, or feel free to tweak this setup for your needs.
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+window.$ = window.jQuery = require('jquery');
 
-const app = new Vue({
-    el: '#app'
+
+// Bind routes to window object
+window.routes = require('./routes');
+
+// Populate pace object
+const pace = require('./vendor/pace');
+
+require('bootstrap-sass');
+require('bootstrap-notify');
+require('bootstrap-datepicker');
+require('./vendor/jquery.chained');
+require('jquery-slimscroll');
+require('datatables.net');
+require('datatables.net-bs');
+require('datatables.net-buttons');
+require('datatables.net-buttons-bs');
+require('datatables.net-buttons/js/buttons.html5');
+require('datatables.net-buttons/js/buttons.colVis');
+require('./vendor/datatables.net-buttons.server-side');
+require('datatables.net-responsive');
+require('datatables.net-responsive-bs');
+require('datatables.net-keytable');
+require('fastclick');
+require('select2');
+require('icheck');
+require('admin-lte');
+
+$(function () {
+    // Bind Select2 menus
+    $('.select2').select2({
+        placeholder: "Select Option"
+    });
+
+    $('.datepicker').datepicker({
+        format: 'yyyy-mm-dd',
+        autoclose: true
+    });
+
+    // Custom Datatables length change select menu
+    oTable = $('#dataTableBuilder').DataTable();
+    $('#dataTableBuilderLengthChanger').on('change.DT', function () {
+        oTable.page.len($(this).val()).draw();
+    });
 });
---
+++ webpack.mix.js
@@ -0,0 +1,32 @@
+const { mix } = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+
+mix
+    .sass('resources/assets/sass/vendor.scss', 'public/css/vendor.css')
+    .sass('resources/assets/sass/app.scss', 'public/css/app.css')
+    .js('resources/assets/js/app.js', 'public/js/app.js')
+    // Prevents Mix's automatic configuring of jQuery
+    // https://github.com/JeffreyWay/laravel-mix/issues/229#issuecomment-276230983
+    .autoload({})
+    .extract([
+        'datatables.net',
+        'datatables.net-bs',
+        'datatables.net-buttons',
+        'datatables.net-buttons-bs',
+        'datatables.net-buttons/js/buttons.colVis.js',
+        'datatables.net-responsive',
+        'datatables.net-responsive-bs',
+        'datatables.net-keytable'
+    ], 'public/js/vendor.js')
+    .version();
