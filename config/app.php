5e6477941a7c28a2fdcf594f9c5f959990c4f9d1
Abdelrahman Omran <me@omranic.com>
2017-02-11 16:19:54 +0200

    Drop default password facade in favor for rinvex/fort

--- config/app.php
+++ config/app.php
@@ -214,7 +214,6 @@ return [
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
-        'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
         'Redis' => Illuminate\Support\Facades\Redis::class,
