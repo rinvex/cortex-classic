60e535a56177527a7f4ed08213cda2688e9fe38f
Abdelrahman Omran <me@omranic.com>
2017-02-11 16:22:01 +0200

    Add package config files

---
+++ config/laroute.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * NOTICE OF LICENSE
+ *
+ * Part of the Rinvex Cortex Application.
+ *
+ * This source file is subject to The MIT License (MIT)
+ * that is bundled with this package in the LICENSE file.
+ *
+ * Package: Rinvex Cortex Application
+ * License: The MIT License (MIT)
+ * Link:    https://rinvex.com
+ */
+
+return [
+
+    /*
+     * The destination path for the javascript file.
+     */
+    'path' => 'resources/assets/js',
+
+    /*
+     * The destination filename for the javascript file.
+     */
+    'filename' => 'routes',
+
+    /*
+     * The namespace for the helper functions. By default this will bind them to
+     * `window.laroute`.
+     */
+    'namespace' => 'routes',
+
+    /*
+     * Generate absolute URLs
+     *
+     * Set the Application URL in config/app.php
+     */
+    'absolute' => false,
+
+    /*
+     * The Filter Method
+     *
+     * 'all' => All routes except "'laroute' => false"
+     * 'only' => Only "'laroute' => true" routes
+     * 'force' => All routes, ignored "laroute" route parameter
+     */
+    'filter' => 'all',
+
+    /*
+     * Controller Namespace
+     *
+     * Set here your controller namespace (see RouteServiceProvider -> $namespace) for cleaner action calls
+     * e.g. 'App\Http\Controllers'
+     */
+    'action_namespace' => '',
+
+    /*
+     * The path to the template `laroute.js` file. This is the file that contains
+     * the ported helper Laravel url/route functions and the route data to go
+     * with them.
+     */
+    'template' => 'vendor/lord/laroute/src/templates/laroute.js',
+
+    /*
+     * Appends a prefix to URLs. By default the prefix is an empty string.
+    *
+    */
+    'prefix' => '',
+
+];
