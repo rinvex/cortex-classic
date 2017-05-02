0c6d55b554938f98d38a71691d5961efa4619237
Abdelrahman Omran <me@omranic.com>
2017-05-02 03:48:14 +0200

    Publish missing vendor config files

---
+++ config/laravel-fractal.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Serializer
+    |--------------------------------------------------------------------------
+    |
+    | The default serializer to be used when performing a transformation. It
+    | may be left empty to use Fractal's default one. This can either be a
+    | string or a League\Fractal\Serializer\SerializerAbstract subclass.
+    |
+    */
+
+    'default_serializer' => '',
+
+];
---
+++ config/rinvex.tenantable.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tenant Database Tables
+    |--------------------------------------------------------------------------
+    |
+    | Specify database table names that should be used to
+    | store your data. You may use whatever you like.
+    |
+    */
+
+    'tables' => [
+
+        'tenants' => 'tenants',
+        'tenantables' => 'tenantables',
+
+    ],
+
+];
