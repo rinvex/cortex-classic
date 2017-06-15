ce22983cc6c2f2244cc4e3efa130736366133d0f
Abdelrahman Omran <me@omranic.com>
2017-06-15 18:25:30 +0200

    Override core Laravel Application class to fix the reverse order of provider registration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 |
 */
 
-$app = new Illuminate\Foundation\Application(
+$app = new Cortex\Foundation\Overrides\Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
 
