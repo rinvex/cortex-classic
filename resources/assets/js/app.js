3b8e133f586fcf2e750ca89297b40e4ed0d24866
Abdelrahman Omran <me@omranic.com>
2017-08-06 17:56:48 +0200

    Drop lodash support

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,13 +1,5 @@
 try {
     /**
-     * First we will load all of this project's JavaScript dependencies which
-     * includes Vue and other libraries. It is a great starting point when
-     * building robust, powerful web applications using Vue and Laravel.
-     */
-
-    window._ = require('lodash');
-
-    /**
      * We'll load jQuery and the Bootstrap jQuery plugin which provides support
      * for JavaScript based Bootstrap features such as modals and tabs. This
      * code may be modified to fit the specific needs of your application.
