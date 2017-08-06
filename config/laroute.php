6a0ca595ee8b4387f02616b82cdcdc1a34e54f98
Abdelrahman Omran <me@omranic.com>
2017-08-07 01:50:00 +0200

    Tweak laroute default paths for interoperability

--- config/laroute.php
+++ config/laroute.php
@@ -7,7 +7,7 @@ return [
     /*
      * The destination path for the javascript file.
      */
-    'path' => 'resources/assets/js',
+    'path' => __DIR__.'/../resources/assets/js',
 
     /*
      * The destination filename for the javascript file.
@@ -49,7 +49,7 @@ return [
      * the ported helper Laravel url/route functions and the route data to go
      * with them.
      */
-    'template' => 'resources/assets/js/vendor/laroute.js',
+    'template' => __DIR__.'/../resources/assets/js/vendor/laroute.js',
 
     /*
      * Appends a prefix to URLs. By default the prefix is an empty string.
