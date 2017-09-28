8b198fc1ec3aee50f5d9a380fb958e03c53ad15f
Abdelrahman Omran <me@omranic.com>
2017-09-28 22:11:51 +0200

    Add tenantarea route prefix config option

--- config/cortex.foundation.php
+++ config/cortex.foundation.php
@@ -17,12 +17,13 @@ return [
 
         // Automatically add a trailing slash to the end of all routes
         'trailing_slash' => true,
-        
-        // defines the URL prefixes for the different areas
+
+        // Defines the URL prefixes for the different areas
         // Changing this option require re-caching routes if already cached
         'prefix' => [
             'memberarea' => 'memberarea',
-            'adminarea' => 'adminarea'
+            'tenantarea' => 'tenantarea',
+            'adminarea' => 'adminarea',
         ],
 
     ],
