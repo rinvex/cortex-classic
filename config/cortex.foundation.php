f2998eed02acc6ac63562952503a9fca9ca160e2
Abdelrahman Omran <me@omranic.com>
2017-09-28 19:05:08 +0000

    Apply fixes from StyleCI

--- config/cortex.foundation.php
+++ config/cortex.foundation.php
@@ -17,12 +17,12 @@ return [
 
         // Automatically add a trailing slash to the end of all routes
         'trailing_slash' => true,
-        
+
         // defines the URL prefixes for the different areas
         // Changing this option require re-caching routes if already cached
         'prefix' => [
             'memberarea' => 'memberarea',
-            'adminarea' => 'adminarea'
+            'adminarea' => 'adminarea',
         ],
 
     ],
