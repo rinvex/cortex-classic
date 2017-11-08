7bac8e551d525c9c8718eb86875fdb2c5228d1f0
Abdelrahman Omran <me@omranic.com>
2017-11-08 11:04:33 +0000

    Apply fixes from StyleCI

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -58,30 +58,30 @@ window.Terminal = require('../../../app/cortex/console/resources/assets/js/termi
 // Theme
 require('admin-lte');
 
-
-$(function () {
+$(function() {
     // Color Picker
     $('.color-picker').colorpicker();
 
-
     // Highlight first tab that has errored inputs
     if ($('.tab-pane .has-error').length) {
-        let erroredTabId = $('.tab-pane .has-error').closest('.tab-pane').attr('id');
+        let erroredTabId = $('.tab-pane .has-error')
+            .closest('.tab-pane')
+            .attr('id');
         $('.nav a[href="#' + erroredTabId + '"]').tab('show');
     }
 
-
     // Slugify
-    $('[data-slugify]').on('input', function (e) {
+    $('[data-slugify]').on('input', function(e) {
         if ($($(this).data('slugify')).val().length === 0) {
-            $(this).on('keyup', function () {
+            $(this).on('keyup', function() {
                 let input = $(this).data('slugify');
-                let slug = $(this).val().slugify();
+                let slug = $(this)
+                    .val()
+                    .slugify();
                 $(input).val(slug);
             });
         }
     });
-
 
     // Enable linkable to tab
     let tabUrl = document.location.toString();
@@ -90,18 +90,16 @@ $(function () {
     }
 
     // Change URL hash for page-reload
-    $('.nav-tabs a').on('shown.bs.tab', function (e) {
+    $('.nav-tabs a').on('shown.bs.tab', function(e) {
         let yScroll = document.body.scrollTop;
         window.location.hash = e.target.hash;
         document.body.scrollTop = yScroll;
     });
 
-
     // Bind Select2 menus
     $('.select2').select2({
-        placeholder: "Select Option"
+        placeholder: 'Select Option',
     });
-
 
     // Configure datepicker
     $('.datepicker').attr('readonly', true);
@@ -109,27 +107,25 @@ $(function () {
         autoApply: true,
         showDropdowns: true,
         singleDatePicker: true,
-        locale: {format: 'YYYY-MM-DD'},
+        locale: { format: 'YYYY-MM-DD' },
     });
 
-    $('.datepicker').on('apply.daterangepicker', function (ev, picker) {
+    $('.datepicker').on('apply.daterangepicker', function(ev, picker) {
         $(this).val(picker.startDate.format('YYYY-MM-DD'));
     });
 
-    $('.datepicker').on('cancel.daterangepicker', function (ev, picker) {
+    $('.datepicker').on('cancel.daterangepicker', function(ev, picker) {
         $(this).val('');
     });
 
-
     // Custom Datatables length change select menu
     oTable = $('.dataTableBuilder').DataTable();
-    $('.dataTableBuilderLengthChanger').on('change.DT', function () {
+    $('.dataTableBuilderLengthChanger').on('change.DT', function() {
         oTable.page.len($(this).val()).draw();
     });
 
-
     // Format Log DataTable details
-    window.dtFormatLogDetails = function (data) {
+    window.dtFormatLogDetails = function(data) {
         if ($.isEmptyObject(data.attributes)) {
             return 'Saved without any changes!';
         }
@@ -139,16 +135,16 @@ $(function () {
         let $tbody = $('<tbody />');
         let $table = $('<table class="table table-condensed table-striped table-bordered" />');
 
-        $.each(data, function (i, item) {
+        $.each(data, function(i, item) {
             let $tr = $('<tr />');
             let $trH = $('<tr />');
 
-            if (! headerDrwan) {
+            if (!headerDrwan) {
                 // Empty cell
                 $trH.append('<td />');
 
                 // Draw columns
-                $.each(item, function (i2, item2) {
+                $.each(item, function(i2, item2) {
                     $trH.append('<td><strong>' + i2 + '</strong></td>');
                 });
 
@@ -163,15 +159,15 @@ $(function () {
             $tbody.appendTo($table);
             $tr.append('<td><strong>' + i + '</strong></td>');
 
-            $.each(item, function (i2, item2) {
+            $.each(item, function(i2, item2) {
                 // Check if cell value is object
                 if (item2 && typeof item2 === 'object') {
                     let cellValue = '';
 
                     // Loop through cell value object and append as string
-                    $.each(item2, function (i3, item3) {
+                    $.each(item2, function(i3, item3) {
                         cellValue += i3 + ': ' + item3 + ' ';
-                    })
+                    });
 
                     // Append cell value to table row
                     $tr.append('<td>' + cellValue + '</td>');
@@ -184,50 +180,47 @@ $(function () {
 
         // Return table HTML string
         return $table;
-    }
+    };
 
-
-    $('#guestarea-register-tenant-process').keypress(function(e){
-        if(e.keyCode === 13) {
+    $('#guestarea-register-tenant-process').keypress(function(e) {
+        if (e.keyCode === 13) {
             e.preventDefault();
             $('.next').click();
         }
     });
 
     $('#guestarea-register-tenant-process').bootstrapWizard({
-        onNext: function (tab, navigation, index) {
+        onNext: function(tab, navigation, index) {
             return $('#guestarea-register-tenant-process').valid();
         },
         onFinish: function onNext(tab, navigation, index) {
             $('#guestarea-register-tenant-process').submit();
         },
-        onTabClick: function (tab, navigation, index, clickedIndex) {
+        onTabClick: function(tab, navigation, index, clickedIndex) {
             if (clickedIndex > index) {
                 return $('#guestarea-register-tenant-process').valid();
             }
-        }
+        },
     });
 
-
-    $('#guestarea-register-member-process').keypress(function(e){
-        if(e.keyCode === 13) {
+    $('#guestarea-register-member-process').keypress(function(e) {
+        if (e.keyCode === 13) {
             e.preventDefault();
             $('.next').click();
         }
     });
 
     $('#guestarea-register-member-process').bootstrapWizard({
-        onNext: function (tab, navigation, index) {
+        onNext: function(tab, navigation, index) {
             return $('#guestarea-register-member-process').valid();
         },
         onFinish: function onNext(tab, navigation, index) {
             $('#guestarea-register-member-process').submit();
         },
-        onTabClick: function (tab, navigation, index, clickedIndex) {
+        onTabClick: function(tab, navigation, index, clickedIndex) {
             if (clickedIndex > index) {
                 return $('#guestarea-register-member-process').valid();
             }
-        }
+        },
     });
-
 });
