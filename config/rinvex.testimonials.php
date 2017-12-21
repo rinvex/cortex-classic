386b493a5dc11ab05ce9f9e6efc520fab359acb3
Abdelrahman Omran <me@omranic.com>
2017-12-21 21:18:04 +0200

    Require composer package cortex/testimonials

---
+++ config/rinvex.testimonials.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    // Testimonials Database Tables
+    'tables' => [
+        'testimonials' => 'testimonials',
+    ],
+
+    // Testimonials Models
+    'models' => [
+        'testimonial' => \Cortex\Testimonials\Models\Testimonial::class,
+    ],
+
+];
