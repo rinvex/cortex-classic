865ef33a8bfb0bb867b29cd8b14be8ca2250577e
Abdelrahman Omran <me@omranic.com>
2017-12-26 14:48:34 +0200

    Override pace option restartOnPushState to false

--- resources/assets/js/vendor/pace.js
+++ resources/assets/js/vendor/pace.js
@@ -13,7 +13,7 @@
     maxProgressPerFrame: 20,
     easeFactor: 1.25,
     startOnPageLoad: true,
-    restartOnPushState: true,
+    restartOnPushState: false, // Custom Rinvex Modification
     restartOnRequestAfter: 500,
     target: 'body',
     elements: {
