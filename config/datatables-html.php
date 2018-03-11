2a040e940b70c51626682f71365478cab9f68fad
Abdelrahman Omran <me@omranic.com>
2018-03-11 02:23:30 +0200

    Stop casting datatables literal object parameters

--- config/datatables-html.php
+++ config/datatables-html.php
@@ -14,5 +14,5 @@ return [
      * Default condition to determine if a parameter is a callback or not
      * Callbacks needs to start by those terms or they will be casted to string
      */
-    'callback' => ['$', '$.', 'function'],
+    'callback' => ['$', '$.', 'function', '{'],
 ];
