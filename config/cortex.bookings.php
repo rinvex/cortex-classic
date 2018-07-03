73067d6f9f085515b8ad23d922affd2ba75e6916
Abdelrahman Omran <me@omranic.com>
2018-07-03 21:02:10 +0200

    Add and update config files

--- config/cortex.bookings.php
+++ config/cortex.bookings.php
@@ -6,13 +6,14 @@ return [
 
     // Bookings media storage disk
     'media' => [
-        'disk' => 's3-public',
+        'disk' => 'public',
     ],
 
     // Bookings database tables
     'tables' => [
         'rooms' => 'rooms',
         'events' => 'events',
+        'services' => 'services',
     ],
 
     // Bookings Models
@@ -25,6 +26,10 @@ return [
         'event_booking' => \Cortex\Bookings\Models\EventBooking::class,
         'event_ticket' => \Cortex\Bookings\Models\EventTicket::class,
         'event' => \Cortex\Bookings\Models\Event::class,
+        'service' => \Cortex\Bookings\Models\Service::class,
+        'service_rate' => \Cortex\Bookings\Models\ServiceRate::class,
+        'service_addon' => \Cortex\Bookings\Models\ServiceAddon::class,
+        'service_booking' => \Cortex\Bookings\Models\ServiceBooking::class,
     ],
 
 ];
