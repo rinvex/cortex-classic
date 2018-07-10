6d0ea178302aa67c79ce8e20df1abfc68bff29a8
Abdelrahman Omran <me@omranic.com>
2018-07-10 14:34:51 +0200

    Add and update config files

--- config/cortex.bookings.php
+++ config/cortex.bookings.php
@@ -11,25 +11,19 @@ return [
 
     // Bookings database tables
     'tables' => [
-        'rooms' => 'rooms',
         'events' => 'events',
         'services' => 'services',
     ],
 
     // Bookings Models
     'models' => [
-        'room' => \Cortex\Bookings\Models\Room::class,
-        'room_rate' => \Cortex\Bookings\Models\RoomRate::class,
-        'room_addon' => \Cortex\Bookings\Models\RoomAddon::class,
-        'room_booking' => \Cortex\Bookings\Models\RoomBooking::class,
-        'room_availability' => \Cortex\Bookings\Models\RoomAvailability::class,
+        'service' => \Cortex\Bookings\Models\Service::class,
+        'service_rate' => \Cortex\Bookings\Models\ServiceRate::class,
+        'service_booking' => \Cortex\Bookings\Models\ServiceBooking::class,
+        'service_availability' => \Cortex\Bookings\Models\ServiceAvailability::class,
         'event_booking' => \Cortex\Bookings\Models\EventBooking::class,
         'event_ticket' => \Cortex\Bookings\Models\EventTicket::class,
         'event' => \Cortex\Bookings\Models\Event::class,
-        'service' => \Cortex\Bookings\Models\Service::class,
-        'service_rate' => \Cortex\Bookings\Models\ServiceRate::class,
-        'service_addon' => \Cortex\Bookings\Models\ServiceAddon::class,
-        'service_booking' => \Cortex\Bookings\Models\ServiceBooking::class,
     ],
 
 ];
