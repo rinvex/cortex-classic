1f2db7f35606cdff72a6479c6c09ad84a7cecd4a
Abdelrahman Omran <me@omranic.com>
2018-07-17 12:41:15 +0200

    Fix notification issue with turbolinks

--- config/notification.php
+++ config/notification.php
@@ -56,7 +56,7 @@ return [
     | :message - message text.
     |
     */
-    'default_format' => "<script>let notification = function() { $.notify({message: ':message'}, {type: ':type', mouse_over: 'pause', z_index: 9999, animate:{enter: \"animated fadeIn\", exit: \"animated fadeOut\"}}); }; window.addEventListener('turbolinks:load', function () { if (typeof notification === 'function') { notification(); notification = null; } });</script>",
+    'default_format' => "<script>window.notification = function() { $.notify({message: ':message'}, {type: ':type', mouse_over: 'pause', z_index: 9999, animate:{enter: \"animated fadeIn\", exit: \"animated fadeOut\"}}); }; window.addEventListener('turbolinks:load', function () { if (typeof window.notification === 'function') { notification(); window.notification = null; } });</script>",
 
     /*
     |--------------------------------------------------------------------------
