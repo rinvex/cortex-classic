085c4f50341c72a70547616977800201edd2c38f
Abdelrahman Omran <me@omranic.com>
2019-01-03 19:36:30 +0200

    Install axios & lodash npm dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,35 @@
  * code may be modified to fit the specific needs of your application.
  */
 
+window._ = require('lodash');
+
+
+try {
+    // window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
 // Bind variables to window object
 import 'expose-loader?$!expose-loader?jQuery!jquery';
 import 'expose-loader?routes!../../public/js/routes';
