162255b2a11cc745df4f2eec6bf5548383d8adab
Abdelrahman Omran <me@omranic.com>
2019-01-04 06:12:21 +0200

    Disable vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -471,6 +471,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
