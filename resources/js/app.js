85ce1b875d5a0d565e81c50710cdcfb4b491869d
Abdelrahman Omran <me@omranic.com>
2019-03-02 17:22:44 +0400

    Remove duplicate JS require

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,8 +10,6 @@ window._ = require('lodash');
 try {
     // window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
 } catch (e) {}
 
 
