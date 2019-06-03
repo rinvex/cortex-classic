478a14c9fcc7fd26ca1b27728115043b80db1972
Abdelrahman Omran <me@omranic.com>
2019-06-03 18:33:02 +0200

    Update language files

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 
 return [
 
-    'session' => [
-        'required' => 'You must login first!',
-        'expired' => 'Session expired, please login again!',
-    ],
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
