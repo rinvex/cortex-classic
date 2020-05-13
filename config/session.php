c1fa6afe6335693634f3a770bc16aca6906a6eb1
Abdelrahman Omran <me@omranic.com>
2020-05-14 00:14:13 +0400

    Replace str_slug leftover global function with class based counterpart

--- config/session.php
+++ config/session.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Str;
+
 return [
 
     /*
@@ -124,7 +126,7 @@ return [
     |
     */
 
-    'cookie' => env('SESSION_COOKIE', str_slug(env('APP_NAME', 'laravel'), '_').'_session'),
+    'cookie' => env('SESSION_COOKIE', Str::slug(env('APP_NAME', 'laravel'), '_').'_session'),
 
     /*
     |--------------------------------------------------------------------------
