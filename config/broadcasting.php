1bba8fce30a3b06f769f7383947bcf60f1a008a2
Abdelrahman Omran <me@omranic.com>
2020-05-25 00:33:09 +0400

    Change default broadcast driver to pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('BROADCAST_DRIVER', 'null'),
+    'default' => env('BROADCAST_DRIVER', 'pusher'),
 
     /*
     |--------------------------------------------------------------------------
