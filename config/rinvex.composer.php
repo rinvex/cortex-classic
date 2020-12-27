4fdab2d92a558ae5a12c5de135ba3e707a20a927
Abdelrahman Omran <me@omranic.com>
2020-12-28 03:09:34 +0400

    Update rinvex/laravel-composer config

--- config/rinvex.composer.php
+++ config/rinvex.composer.php
@@ -4,24 +4,9 @@ declare(strict_types=1);
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Composer Installer Paths
-    |--------------------------------------------------------------------------
-    |
-    | Here you can specify the installable paths for composer installer.
-    |
-    | CAUSTION: YOU CAN NOT USE ANY LARAVEL FEATURES IN THIS CONFIG FILE!
-    | This config file is loaded first thing at the very begining
-    | on every composer action before even any package
-    | being installed. Just pure PHP acceptable!
-    |
-    | Paths are relative to the root config dir.
-    |
-    */
-
-    'base' => __DIR__.'/../',
-    'app' => __DIR__.'/../app/',
-    'bootstrap' => __DIR__.'/../bootstrap/',
+    'core_modules' => [
+        'cortex/foundation',
+        'cortex/auth',
+    ],
 
 ];
