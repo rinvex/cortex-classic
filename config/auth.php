e188c0da33855d0d37d57975ec9d83c735ce21f1
Abdelrahman Omran <me@omranic.com>
2021-02-25 04:43:52 +0400

    Move email verification config to default project config/auth.php file

--- config/auth.php
+++ config/auth.php
@@ -18,6 +18,7 @@ return [
     'defaults' => [
         'guard' => 'member',
         'passwords' => 'member',
+        'emails' => 'member',
     ],
 
     /*
@@ -127,6 +128,49 @@ return [
     */
 
     'passwords' => [
+
+        'admin' => [
+            'provider' => 'admins',
+            'expire' => 60,
+        ],
+
+        'member' => [
+            'provider' => 'members',
+            'expire' => 60,
+        ],
+
+        'manager' => [
+            'provider' => 'managers',
+            'expire' => 60,
+        ],
+
+        'guardian' => [
+            'provider' => 'guardians',
+            'expire' => 60,
+        ],
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Email Verification
+    |--------------------------------------------------------------------------
+    |
+    | The broker option controls the default email verification broker for
+    | your application. You may change this default as required,
+    | but they're a perfect start for most applications.
+    |
+    | You may need to specify multiple email verification configurations if you have
+    | more than one user table or model in the application and you want to have
+    | separate email verification settings based on the specific user types.
+    |
+    | The expire time is the number of minutes that the email verification token
+    | should be considered valid. This security feature keeps tokens short-lived
+    | so they have less time to be guessed. You may change this as needed.
+    |
+    */
+
+    'emails' => [
 
         'admin' => [
             'provider' => 'admins',
