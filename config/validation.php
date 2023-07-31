dc80f8657d9dc0dbf9eca9456fd552b74eca70ad
Abdelrahman Omran <me@omranic.com>
2023-07-31 11:53:10 +0100

    Move username, email, and password validation rules to config file

---
+++ config/validation.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Validation\Rules\Password;
+
+return [
+
+    'rules' => [
+
+        'password' => config('app.env') === 'production'
+            ? Password::min(8)
+                      ->letters()
+                      ->mixedCase()
+                      ->numbers()
+                      ->symbols()
+                      ->uncompromised()
+            : Password::min(8)
+                      ->letters()
+                      ->mixedCase()
+                      ->numbers(),
+
+        'username' => [
+            'alpha_dash',
+            'max:64',
+            'min:3',
+        ],
+
+        'email' => [
+            config('app.env') === 'production' ? 'email:rfc,dns,spoof' : 'email:rfc',
+            'max:128',
+            'min:3',
+        ],
+
+    ],
+
+];
